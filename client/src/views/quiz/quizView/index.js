8ecedfd1639cb115ed255c61dd843ea0a534133a
Nghia Ta <nghia.ta@asicompany.asia>
2021-05-11 18:01:01 +0700

    update page.

--- client/src/views/quiz/quizView/index.js
+++ client/src/views/quiz/quizView/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import {
     Button,
     Card,
@@ -9,8 +9,8 @@ import {
     makeStyles,
     Radio,
     RadioGroup,
-} from '@material-ui/core';
-import Page from '../../../components/Page';
+} from '@material-ui/core'
+import Page from '../../../components/Page'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -32,53 +32,67 @@ const useStyles = makeStyles((theme) => ({
 
 const QuizView = () => {
     const classes = useStyles();
-    let [quiz, setQuiz] = React.useState({
+    const [quizCollection, setQuizCollection] = React.useState([{
         "question": "",
-        "answers": []
-    });
-    const [error, setError] = React.useState(null);
-    const [index, setIndex] = React.useState(0);
+        "answers": [],
+        "your-answer": "",
+        "correct-answer": ""
+    }])
+    const [quiz, setQuiz] = React.useState(quizCollection[0]);
+    const [error, setError] = React.useState(null)
+    const [index, setIndex] = React.useState(0)
 
     const handleChange = (event) => {
-        let newAnswer = parseInt(event.target.value);
-        let newQuiz = { ...quiz, answer: newAnswer }
-        setQuiz(newQuiz);
+        let newAnswer = parseInt(event.target.value)
+        let newQuiz = { ...quiz, 'your-answer': newAnswer }
+        setQuiz(newQuiz)
     };
 
     useEffect(() => {
-        loadQuiz(index);
+        loadQuizCollection()
+        if (quizCollection.length) {
+            setQuiz(quizCollection[0])
+        }
     }, [])
 
-    const loadQuiz = (i) => {
+    const loadQuizCollection = () => {
         fetch('http://localhost:5000/api/quiz-pro')
             .then(res => res.json())
             .then(
                 (res) => {
                     if (res) {
-                        setQuiz(res[i])
+                        setQuizCollection(res)
                     }
                 },
                 (err) => {
                     setError(err)
+                    alert('error: '+ err)
                 }
             )
     }
 
-    let listAnswerView = '';
-    let next_disabled = index > 1 ? true : false;
-    let back_disabled = index === 0 ? true : false;
+    const loadQuiz = (i) => {
+        setQuiz(quizCollection[i])
+    }
+
+    let listAnswerView = ''
+    let next_disabled = index > 0 ? true : false // need total of questions
+    let back_disabled = index === 0 ? true : false
     if (quiz) {
+        let radioItems = quiz['answers'].map((answer, index) => (
+            <FormControlLabel key={index} value={index} control={<Radio />} label={ String.fromCharCode(index + 65) +'. '+ answer } />
+        ))
         listAnswerView = (
-            <RadioGroup aria-label="gender" name="gender1" value="" onChange={handleChange}>
-                <FormControlLabel value={0} control={<Radio />} label={'A. ' + quiz['answers'][0]} />
-                <FormControlLabel value={1} control={<Radio />} label={'B. ' + quiz['answers'][1]} />
-                <FormControlLabel value={2} control={<Radio />} label={'C. ' + quiz['answers'][2]} />
-                <FormControlLabel value={3} control={<Radio />} label={'D. ' + quiz['answers'][3]} />
+            <RadioGroup aria-label="gender" name="gender1" value={quiz['your-answer']} onChange={handleChange}>
+                { radioItems }
             </RadioGroup>
         );
     }
 
-
+    const loadQuestion = async (index) => {
+        await setIndex(index)
+        await loadQuiz(index)
+    }
 
     return (
         <Page
@@ -94,8 +108,8 @@ const QuizView = () => {
                 </Card>
 
                 <div className={classes.btnStyle}>
-                    <Button variant="contained" color="primary" onClick={() => setIndex(index - 1)} disabled={back_disabled}>Back</Button>
-                    <Button variant="contained" color="primary" onClick={() => setIndex(index + 1)} disabled={next_disabled}>Next</Button>
+                    <Button variant="contained" color="primary" onClick={() => loadQuestion(index - 1)} disabled={back_disabled}>Back</Button>
+                    <Button variant="contained" color="primary" onClick={() => loadQuestion(index + 1)} disabled={next_disabled}>Next</Button>
                 </div>
             </Container>
         </Page>
